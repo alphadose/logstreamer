62896de3265f7b48cc0f58fddebe102a10861f08
Anish Mukherjee <anish.mukherjee1996@gmail.com>
2022-07-08 08:21:55 +0530

    handle edge case and optimize file reader

--- utils/file.go
+++ utils/file.go
@@ -34,18 +34,41 @@ func NewFileReader[T any](filename string) *FileReader[T] {
 // and binds each line to a specific JSON object specified by the generic parameter
 func (r *FileReader[T]) ReadLines(count uint64) ([]*T, error) {
 	jsonData := make([]*T, 0)
+	var (
+		line []byte
+		err  error
+		tmp  *T
+	)
 	for ; count > 0; count-- {
-		line, _, err := r.reader.ReadLine()
+		line, err = r.readEntireLine()
 		if err != nil {
 			return jsonData, err
 		}
-		d := new(T)
-		if err := json.Unmarshal(line, d); err != nil {
+		tmp = new(T)
+		if err = json.Unmarshal(line, tmp); err != nil {
 			return jsonData, err
 		}
-		jsonData = append(jsonData, d)
+		jsonData = append(jsonData, tmp)
 	}
 	return jsonData, nil
+}
+
+// readEntireLine reads the entire line till it reaches a `\n` character or EOF
+// this is required because *bufio.Reader.ReadLine() only returns a maximum of 65536 characters in each line at a time
+// in case the number of characters exceed that, the returned []byte is broken in which case JSON unmarshalling will fail due to invalid syntax
+// the function below ensures the entire line is read and JSON is parsed safely even if a break occurs due to bufio internals
+// Reference -> https://devmarkpro.com/working-big-files-golang
+func (r *FileReader[T]) readEntireLine() (ln []byte, err error) {
+	var (
+		isPrefix = true
+		parts    []byte
+	)
+	ln = make([]byte, 0)
+	for isPrefix && err == nil {
+		parts, isPrefix, err = r.reader.ReadLine()
+		ln = append(ln, parts...)
+	}
+	return ln, err
 }
 
 // Close closes the file reader object
