6e2d0961886b051368af8309e000beac43501380
Anish Mukherjee <anish.mukherjee1996@gmail.com>
2022-07-08 09:50:26 +0530

    fix bug in main

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"flag"
 
-	"github.com/alphadose/logstreamer/grpc"
 	"github.com/alphadose/logstreamer/utils"
 )
 
 func main() {
 	flag.StringVar(&file, "f", "./data.txt", "Absolute path to file")
 	flag.StringVar(&mongoURI, "mongo", "mongodb://localhost:27017", "MongoDB URI")
-	flag.StringVar(&grpcURI, "grpc", "localhost"+grpc.Port, "URI of GRPC server")
+	flag.StringVar(&grpcURI, "grpc", "localhost:3002", "URI of GRPC server")
 	flag.Uint64Var(&batchSize, "bsize", 200, "Batch size of upload operations (restriction helpful in cases of file_size > 16 GB)")
 	flag.BoolVar(&parallel, "parallel", false, "Should storage upload operations run in parallel?")
 	flag.Parse()
